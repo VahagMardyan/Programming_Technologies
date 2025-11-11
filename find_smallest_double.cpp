73d3131b45fe6908e3a10d4dc9eb1c1f31ff80bc
VahagMardyan <137500258+VahagMardyan@users.noreply.github.com>
2025-11-11 19:37:32 +0400

    Delete find_smallest_double.cpp

--- find_smallest_double.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cstdint>
-
-int main() {
-    uint64_t n = 1;
-    double x = *reinterpret_cast<double*>(&n);
-    std::cout<<"The smallest positive double is: "<<x<<std::endl;
-    return 0;
-}
