08877c698639a836e16e6b8f32f8e0aa71894a44
user.email <mardyan2003@gmail.com>
2025-11-29 16:30:44 +0400

    Updated string class

--- string.cpp
+++ string.cpp
@@ -1,148 +1,276 @@
 #include <iostream>
+#include <cstring>
+#include <stdexcept>
 
 class String {
     private:
-        static const int string_size = 23; // for stack
-
-        union {
-            char s[string_size]; // stack-memory (small strings)
-            char *heap; // heap-memory (large strings)
-        };
-        
-        size_t size;
-        size_t capacity;
-        bool inHeap;
-
-        size_t len(const char* str) {
-            size_t l = 0;
-            while(str[l] != '\0') {
-                l++;
-            }
-            return l;
+        static const size_t SSO_CAP=15; // Small String Optimisation Capacity
+        size_t size_;
+        size_t capacity_;
+        bool heap;
+        char* data_;
+        char small_[SSO_CAP + 1];
+        char* data() {
+            return heap ? data_ : small_;
+        } 
+        const char* data() const {
+            return heap ? data_ : small_;
         }
-
-        void copy(char* dest, const char* src) {
-            size_t i=0;
-            while((dest[i]=src[i]) != '\0') {
-                i++;
+        void enable_heap(size_t newcap) {
+            char* newdata = new char[newcap];
+            std::memcpy(newdata, data(), size_ + 1);
+            if(heap) delete[] data_;
+            data_ = newdata;
+            capacity_ = newcap;
+            heap = true;
+        }
+        void ensure_capacity(size_t newsize) {
+            if(!heap && newsize <= SSO_CAP) return;
+            if(!heap) {
+                enable_heap(newsize + 1);
+            } else if(newsize + 1 > capacity_ ) {
+                enable_heap((newsize + 1) * 2);
             }
         }
-    
     public:
-        String(const char* str) {
-            size = len(str);
-            if(size < string_size) {
-                inHeap = false;
-                capacity = string_size - 1;
-                copy(s,str);
+        
+        String() : size_(0), capacity_(SSO_CAP), heap(false), data_(nullptr) {
+            small_[0] = '\0';
+        }
+
+        String(const char* s) {
+            size_ = std::strlen(s);
+            if(size_ <= SSO_CAP) {
+                heap = false;
+                capacity_ = SSO_CAP;
+                std::memcpy(small_, s, size_ + 1);
+                data_ = nullptr;
             } else {
-                inHeap = true;
-                capacity = size;
-                heap = new char[capacity + 1];
-                copy(heap, str);
+                heap = true;
+                capacity_ = size_ + 1;
+                data_ = new char[capacity_];
+                std::memcpy(data_, s, size_ + 1);
             }
         }
 
-        String() {}
+        // // Copy constructor
+        String(const String& other) {
+            size_ = other.size_;
+            if(!other.heap) {
+                heap = false;
+                capacity_ = SSO_CAP;
+                std::memcpy(small_, other.small_, size_ + 1);
+                data_ = nullptr;
+            } else {
+                heap = true;
+                capacity_ = other.capacity_;
+                data_ = new char[capacity_];
+                std::memcpy(data_, other.data_, size_ + 1);
+            }
+        }
 
+        // // Move constructor
+        String(String&& other) noexcept : size_(other.size_), 
+            capacity_(other.capacity_), heap(other.heap), data_(other.data_) {
+                if(!other.heap) {
+                    std::memcpy(small_, other.small_, size_ + 1);
+                    data_ = nullptr;
+                }
+                other.size_ = 0;
+                other.heap = false;
+                other.small_[0] = '\0';
+                other.data_ = nullptr;
+                other.capacity_ = SSO_CAP;
+        }
+
+        // // Destructor
         ~String() {
-            if(inHeap) {
-                delete[] heap;
-            }
-        }
-
-        // // copy constructor
-        String(const String& other_str) {
-            size = other_str.size;
-            capacity = other_str.capacity;
-            inHeap = other_str.inHeap;
-            if(inHeap) {
-                heap = new char[capacity + 1];
-                copy(heap, other_str.heap);
-            } else {
-                copy(s, other_str.s);
+            if(heap) {
+                delete[] data_;
             }
         }
 
         String& operator=(const String& other) {
-            if(this == &other) {
-                return *this;
-            }
-            if(inHeap) {
-                delete[] heap;
-            }
-            size = other.size;
-            capacity = other.capacity;
-            inHeap = other.inHeap;
-            if(inHeap) {
-                heap = new char[capacity + 1];
-                copy(heap, other.heap);
+            if(this == &other) return *this;
+            if(heap) delete[] data_;
+            size_ = other.size_;
+            if(!other.heap) {
+                heap = false;
+                capacity_ = SSO_CAP;
+                std::memcpy(small_, other.small_, size_ + 1);
+                data_ = nullptr;
             } else {
-                copy(s,other.s);
+                heap = true;
+                capacity_ = other.capacity_;
+                data_ = new char[capacity_];
+                std::memcpy(data_, other.data_, size_ + 1);
             }
             return *this;
         }
-        char& operator[](size_t i) {
-            return inHeap ? heap[i] : s[i];
-            // return inHeap ? *(heap + i) : *(s + i);
-        }
-        const char* c_str() const {
-            return inHeap ? heap : s;
-        }
-        size_t length() const {
-            return size;
-        }
-        String operator + (const String& other) const {
-            String result;
-            size_t new_size = size + other.size;
-            if(new_size < string_size) {
-                result.inHeap = false;
-                result.capacity = string_size - 1;
-                result.size = new_size;
 
-                size_t i = 0, j = 0;
-                while(i < size) {
-                    result.s[i] = inHeap ? heap[i] : s[i];
-                    i++;
-                }
-                while(j < other.size) {
-                    result.s[i] = other.inHeap ? other.heap[j] : other.s[j];
-                    j++;
-                    i++;
-                }
-                result.s[i] = '\0';
+        String& operator=(String&& other) noexcept {
+            if(this == &other) return *this;
+            if(heap) delete[] data_;
+            size_ = other.size_;
+            capacity_ = other.capacity_;
+            heap = other.heap;
+            if(!other.heap) {
+                std::memcpy(small_, other.small_, size_ + 1);
+                data_ = nullptr;
             } else {
-                result.inHeap = true;
-                result.capacity = new_size;
-                result.size = new_size;
-                result.heap = new char[new_size + 1];
-                size_t i = 0, j = 0;
-                while(i < size) {
-                    result.heap[i] = inHeap ? heap[i] : s[i];
-                    i++;
-                }
-                while(j < other.size) {
-                    result.heap[i] = other.inHeap ? other.heap[j] : other.s[j];
-                    j++;
-                    i++;
-                }
-                result.heap[i] = '\0';
+                data_ = other.data_;
             }
-            return result;
+            other.size_ = 0;
+            other.heap = false;
+            other.small_[0] = '\0';
+            other.data_ = nullptr;
+            other.capacity_ = SSO_CAP;
+            return *this;
+        }
+
+        bool empty() {
+            return size_ == 0;
+        }
+
+        size_t size() const {
+            return size_;
+        }
+
+        const char* c_str() const {
+            return data();
+        }
+
+        char& operator[](size_t index) {
+            return data()[index];
+        }
+
+        const char& operator[](size_t index) const {
+            return data()[index];
+        }
+
+        char at(size_t index) const {
+            if(index >= size_) {
+                throw std::out_of_range("Index out of range");
+            }
+            return data()[index];
+        }
+
+        void append(const String& s) {
+            size_t newsize = size_ + s.size_;
+            ensure_capacity(newsize);
+            std::memcpy(data() + size_, s.data(), s.size_ + 1);
+            size_ = newsize;
+        }
+
+        void append(char c) {
+            ensure_capacity(size_ + 1);
+            data()[size_] = c;
+            size_ ++;
+            data()[size_] = '\0';
+        }
+
+        String& operator+=(const String& s) {
+            append(s);
+            return *this;
+        }
+        String& operator+=(char c) {
+            append(c);
+            return *this;
+        }
+
+        String operator+(const String& other) const {
+            String out;
+            out.size_ = size_ + other.size_;
+            if(out.size_ <= SSO_CAP) {
+                out.heap = false;
+                out.capacity_ = SSO_CAP;
+                std::memcpy(out.small_, data(), size_);
+                std::memcpy(out.small_ + size_, other.data(), other.size_ + 1);
+            } else {
+                out.heap = true;
+                out.capacity_ = out.size_ + 1;
+                out.data_ = new char[out.capacity_];
+                std::memcpy(out.data_, data(), size_);
+                std::memcpy(out.data_ + size_, other.data(), other.size_ + 1);
+            }
+            return out;
+        }
+
+        int find(char c) const {
+            for(int i=0;i<size_; ++i) {
+                if(data()[i] == c) return i;
+            }
+            return -1;
+        }
+
+        int find(const String& sub) const {
+            if(sub.size_ == 0) return 0;
+            if(sub.size_ > size_) return -1;
+            for(int i=0; i <= size_ - sub.size_; ++i) {
+                if(std::memcmp(data() + i, sub.data(), sub.size_) == 0) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        void insert(const String& s, int pos) {
+            if(pos > size_) pos = size_;
+            size_t newsize = size_ + s.size_;
+            ensure_capacity(newsize);
+            char* d = data();
+            std::memmove(d + pos + s.size_, d + pos, size_ - pos);
+            std::memcpy(d + pos, s.data(), s.size_);
+            size_ = newsize;
+            d[size_] = '\0';
+        }
+
+        void erase(size_t start, size_t end) {
+            if(start >= size_ || start > end) return;
+            if(end >= size_) end = size_ - 1;
+            size_t count = end - start + 1;
+            char* d = data();
+            std::memmove(d + start, d + start + count, size_ - (end + count));
+            size_ -= count;
+            d[size_] = '\0';
+        }
+
+        void reverse() {
+            char *d = data();
+            for(size_t i=0; i < size_ / 2; ++i) {
+                std::swap(d[i], d[size_ - 1 - i]);
+            }
+        }
+
+        void clear() {
+            if(heap) {
+                delete[] data_;
+                heap = false;
+                capacity_ = SSO_CAP;
+            }
+            size_ = 0;
+            small_[0] = '\0';
+            data_ = nullptr;
+        }
+
+        void print() const {
+            std::cout<<data()<<std::endl;
         }
 };
 
 int main() {
-    String a = "Hello";
-    String b = "World";
-    String x = "a";
-    String d = "abcdefghijklmnopqrstuvwxyz";
-    String c = a + " "  + b + "!!!";
-    d[2] = 'A';
-    std::cout<<sizeof(x)<<std::endl; // 48
-    std::cout<<d.c_str()<<std::endl; // abAdefghijklmnopqrstuvwxyz
-    std::cout<<sizeof(d)<<std::endl; // 48
-    // std::cout<<std::boolalpha<<d.inHeap<<std::endl; // when inHeap is public -> true
-    std::cout<<c.length()<<std::endl;
+    String x = "Hello World, I am Vahag";// true
+    x.append(" !!!");
+    size_t s = x.size();
+    x+='A';
+    x+="BCD";
+    x.print();
+    x.erase(2,5);
+    x.print();
+    x = "ABCD";
+    x.print();
+    x.reverse();
+    // x.clear();
+    x.print();
     return 0;
 }
