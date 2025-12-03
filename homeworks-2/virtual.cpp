3e8f98427e6105c89e579d02d66fca935f4bfabb
VahagMardyan <mardyan2003@gmail.com>
2025-12-03 21:54:16 +0400

    Manual dynamic cast

--- homeworks-2/virtual.cpp
+++ homeworks-2/virtual.cpp
@@ -5,8 +5,12 @@
 class Base {
     public:
         void (*fooPtr)(Base*, int); // VirtualTable
+        const void* typePtr;
+        static const int typeMarker;
+
         Base() {
             fooPtr = &Base::fooImpl;
+            typePtr = &typeMarker;
         }
         static void fooImpl(Base* self, int x) {
             std::cout<<"Base: "<<x<<std::endl;
@@ -16,10 +20,14 @@ class Base {
         }
 };
 
+const int Base::typeMarker = 0;
+
 class D : public Base {
     public:
+        static const int typeMarker;
         D() {
             fooPtr = &D::fooImpl;
+            typePtr = &typeMarker;
         }
         static void fooImpl(Base* self, int x) {
             std::cout<<"Derived: "<<x<<std::endl;
@@ -28,11 +36,14 @@ class D : public Base {
             fooPtr(this, x);
         }
 };
+const int D::typeMarker = 0;
 
 class D1 : public Base {
     public:
+        static const int typeMarker;
         D1() {
             fooPtr = &D1::fooImpl;
+            typePtr = &typeMarker;
         }
         static void fooImpl(Base* self, int x) {
             std::cout<<"Derived1: "<<x<<std::endl;
@@ -41,6 +52,18 @@ class D1 : public Base {
             fooPtr(this, x);
         }
 };
+const int D1::typeMarker = 0;
+
+template <typename T>
+T* dynamicCast(Base* obj, const void* expectedType) {
+    if(obj->typePtr == expectedType) {
+        return reinterpret_cast<T*>(obj);
+    }
+    return nullptr;
+}
+
+#define dynamic_cast_D(ptr) dynamicCast<D>(ptr, &D::typeMarker)
+#define dynamic_cast_D1(ptr) dynamicCast<D1>(ptr, &D1::typeMarker)
 
 int main() {
     Base *obj = new D();
@@ -62,6 +85,33 @@ int main() {
     // // cast is UNSAFE
     Base *d11 = new D1();
     ((D1*)d11)->foo(); // Derived1 456
+
+    Base* b1 = new D();
+    Base* b2 = new D1();
+    Base* b3 = new Base();
+
+    // safe downcast
+    if(auto p = dynamic_cast_D(b1)) {
+        p->foo(); // Derived: 100
+    }
+
+    // wrong type -> nullptr
+    if(auto p = dynamic_cast_D(b2)) {
+        std::cout<<"Should not print"<<std::endl;
+    } else {
+        std::cout<<"dynamic_cast_D(b2)=nullptr (safe)"<<std::endl;
+    }
+
+    // cast to D1
+    if(auto p = dynamic_cast_D1(b2)) {
+        p->foo(); // Derived1: 456
+    }
+
+    // base object -> cast fails
+    if(!dynamic_cast_D(b3)) {
+        std::cout << "Cannot cast Base to D (correct)"<<std::endl;
+    }
+
     return 0;
 }
 
